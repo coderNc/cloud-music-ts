6006623be86f1c70509488da11b48cbe66e7cd57
destiny <895407322@qq.com>
2024-08-18 18:44:44 +0800

    fix: api修改

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: '/cloud/',
   plugins: [react()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
@@ -16,7 +17,6 @@ export default defineConfig({
     proxy: {
       // 选项写法
       '/api': {
-        // target: 'http://62.234.48.139:3000/',
         target: 'http://www.niuchao.top:3000/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
